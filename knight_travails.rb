b8c3e1d3bf23534123acd7f80582a52f18637070
Babatunde Alli <tunsmart@gmail.com>
2022-03-17 14:12:30 -0400

    Refactoring

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,4 +1,4 @@
-require 'pry-byebug'
+#offsets to ensure knight moves don't go offboard
 MOVE_OFFSETS = [[-1,-2],[1,-2],
                 [-2,-1], [2,-1],
                 [-2,1], [2,1],
@@ -26,6 +26,7 @@ def build_graph
     graph
 end
 
+#uses the offsets to determine legal moves and only yields that to the graph builder
 def legal_moves_from(square)
     MOVE_OFFSETS.each{|offset_arr|
         legal_row = square[0] + offset_arr[0]
@@ -34,7 +35,7 @@ def legal_moves_from(square)
     }
 end
 
-class VertexProperties
+class NodeProperties
     attr_accessor :distance, :predecessor
     def initialize
         distance = nil
@@ -48,20 +49,19 @@ def do_bfs(source)
     bfs_info = {}
     keys = graph.keys
     for i in 0...keys.length do
-        bfs_info[keys[i]] = VertexProperties.new
+        bfs_info[keys[i]] = NodeProperties.new
     end
     bfs_info[source].distance = 0
     queue = []
     queue << source
     until queue.empty?
-        vertex = queue[0]
+        node = queue[0]
         
-        to_queue = graph[vertex]
+        to_queue = graph[node]
         until to_queue.empty?
-            #binding.pry
             if bfs_info[to_queue[0]].distance.nil?
-                bfs_info[to_queue[0]].predecessor = vertex 
-                bfs_info[to_queue[0]].distance = bfs_info[vertex].distance + 1
+                bfs_info[to_queue[0]].predecessor = node 
+                bfs_info[to_queue[0]].distance = bfs_info[node].distance + 1
                 queue << to_queue.shift
             else
                 to_queue.shift
@@ -83,12 +83,12 @@ end
 
 def find_path(square, search)
     result = []
-    vertex = square
-    until search[vertex].predecessor.nil?
-        result << vertex
-        vertex = search[vertex].predecessor
+    node = square
+    until search[node].predecessor.nil?
+        result << node
+        node = search[node].predecessor
     end
-    result << vertex
+    result << node
     result.reverse!
 end
 
@@ -96,7 +96,7 @@ end
 #moves.each{|key,value| puts "#{key}, #{value}"}
 #info = do_bfs(moves, [0,0])
 #info.each do |key, value|
-#    puts "vertex: #{key}, distance: #{value.distance}, predecessor: #{value.predecessor}"
+#    puts "node: #{key}, distance: #{value.distance}, predecessor: #{value.predecessor}"
 #end
 
 knight_move([0,0],[3,3])
